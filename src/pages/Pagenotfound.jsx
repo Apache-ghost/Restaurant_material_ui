e1a5de81c25fff8768c8393f4e0a0958e6be6743
Guegouo M. Guiddel <guegouomoghommahieguiddel@gmail.com>
2024-07-25 17:57:40 +0200

    First commit

---
+++ src/pages/Pagenotfound.jsx
@@ -0,0 +1,12 @@
+
+import Layout from "../components/Layout/Layout";
+
+const Pagenotfound = () => {
+  return (
+    <Layout>
+      <h1>Page Not Found</h1>
+    </Layout>
+  );
+};
+
+export default Pagenotfound;
